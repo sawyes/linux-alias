14067a1e18dba75fdb90e5af9d8f25c003691578
peter <7061384@126.com>
2018-10-24 00:00:26 +0800

    add aliases file and init shell

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ cp .aliases aliases
 echo "==> copy done ! \r\n";
 
 if [ -f ~/.bash_aliases ] ; then
-    echo aliases-file >> ~/.bashrc;
+    cat aliases-file >> ~/.bashrc;
 	echo "==> append aliases-file done ! \r\n";
 fi
 
