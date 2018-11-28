880a30f28b8f2b4f91829c8369a325cf1f532755
peter <pec_wu@patozon.net>
2018-11-28 11:54:21 +0800

    fix

--- init.sh
+++ init.sh
@@ -8,8 +8,10 @@ if [ -f ~/.bash_aliases ] ; then
     cat aliases-file >> ~/.bash_aliases;
     source ~/.bash_aliases;
     echo "==> append aliases-file to  ~/.bash_aliases ! \r\n";
+    echo "==> source ~/.bash_aliases \r\n";
 else
     cat aliases-file >> ~/.bashrc;
-    source ~/.bashrc
+
     echo "==> append aliases-file to  ~/.bashrc ! \r\n";
+    echo "==> source ~/.bashrc \r\n";
 fi
